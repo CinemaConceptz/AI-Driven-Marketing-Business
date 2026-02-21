50046912cafd84f1036919829a8aa54389b6a4e4
Cinema Conceptz <cinemaconceptz@gmail.com>
2026-02-20 22:54:08 -0600

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,6 +5,28 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://*.firebaseapp.com https://*.gstatic.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: blob: https://*.googleapis.com https://*.firebasestorage.app https://firebasestorage.googleapis.com",
+              "media-src 'self' blob: data: https://*.googleapis.com https://*.firebasestorage.app https://firebasestorage.googleapis.com",
+              "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebasestorage.app https://firebasestorage.googleapis.com wss://*.firebaseio.com https://*.sentry.io https://*.ingest.sentry.io",
+              "frame-src 'self' https://*.firebaseapp.com https://accounts.google.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withSentryConfig(nextConfig, {
